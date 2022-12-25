e4270ac9c296fcc78f066a7f231b5798a45ad08d
shoham sofer <92480018+sofer1445@users.noreply.github.com>
2022-12-25 13:45:57 +0200

    ראשון 13:45

--- src/TablesPage.js
+++ src/TablesPage.js
@@ -1,41 +1,79 @@
 import react from 'react';
+import axios from "axios";
+
 const api = "https://app.seker.live/fm1"
 
 class TablePage extends react.Component {
     state = {
         data: [],
-        itemOfLeagues : {
-            country: "",
-            league: ""
+        leagueTable: [],
+    }
+
+    componentDidMount() {
+        this.inputLeaguesFromApi();
+    }
 
 
-        }
+    inputLeaguesFromApi = () => {
+        axios.get(api + "/leagues")
+            .then(response => {
+                    this.setState({
+                        data: response.data
+                    })
+                }
+            )
 
     }
+    leagueTables = (id) => {
+        axios.get(api + "/teams/" + id)
+            .then(response => {
+                    this.setState({
+                        leagueTable: response.data
+                    })
+                }
+            )
+    }
+
     render() {
         return (
             <div>
-                <h1>TableOfLeague</h1>
-                <table className ={"Leagues"}>
+                <h1>Table Of League</h1>
+                <table className={"Countries"}>
                     <tr>
                         <th>countries</th>
-                        <th>leagues</th>
-
-
                     </tr>
                     {this.state.data.map((item, index) => {
                             return (
-                            <tr>
-                                <td>{item.country}</td>
-                                <td>{item.league}</td>
-                            </tr>
-                        )
-                    }
+                                <tr key={index}>
+                                    <td onClick={() => this.leagueTables(item.id)}>{item.name}</td>
+                                </tr>
+                            )
+                        }
                     )}
                 </table>
+                <table className={"LeagueTable"}>
+                    <div>
 
+                        <tr>
+                            <th> The league table online</th>
+
+                        </tr>
+
+                    </div>
+                    {this.state.leagueTable.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.name}</td>
+                                </tr>
+
+                            )
+                        }
+                    )}
+
+                </table>
 
             </div>
+
         )
     }
 }

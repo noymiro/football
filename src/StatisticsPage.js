be94bfcafef2394739df2582342d5fe07945a6f1
shoham sofer <92480018+sofer1445@users.noreply.github.com>
2023-01-03 12:48:20 +0200

    שלב 4

--- src/StatisticsPage.js
+++ src/StatisticsPage.js
@@ -1,9 +1,60 @@
-function StatisticsPage () {
-    return(
-        <div>
-            StatisticsPage
-        </div>
-    )
+import React from "react";
+import axios from "axios";
+const api = "https://app.seker.live/fm1";
+
+class StatisticsPage extends React.Component {
+    state = {
+        data: [],
+        firstHalfGoals: [],
+        secondHalfGoals: [],
+        arrayOfRounds: new Map(),
+        roundOfTheMostGoals: 0,
+        roundOfTheLeastGoals: 0,
+
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+    getUrlOfLeague = async  (leagueId) => {
+        const url = api + "/history/" + leagueId;
+        const response = await axios.get(url);
+        this.setState({
+            ...this.state,
+            data: response.data,
+        })
+
+
+
+
+    }
+
+    sortGoals () {
+        const data = this.state.data;
+        const firstHalfGoals = [];
+        const secondHalfGoals = [];
+        data.map((game) => {
+            game.goals.map((goal) => {
+                if (goal.half === "first") {
+                    firstHalfGoals.push(goal);
+                } else {
+                    secondHalfGoals.push(goal);
+                }
+            })
+        })
+        this.setState({
+            ...this.state,
+            firstHalfGoals: firstHalfGoals,
+            secondHalfGoals: secondHalfGoals,
+        })
+
+    }
+
+
 }
 
 export default StatisticsPage;

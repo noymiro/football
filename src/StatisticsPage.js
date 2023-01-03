652a0fd230de1042d389f9bc209124c8a9e36d66
tsvi2005 <tsvi2005@gmail.com>
2023-01-03 16:31:01 +0200

    update

--- src/StatisticsPage.js
+++ src/StatisticsPage.js
@@ -33,33 +33,52 @@ class StatisticsPage extends React.Component {
 
     }
 
-    sortGoals () {
+    async sortGoals() {
+        const url = api + "/history/" + leagueId;
+        const response = await axios.get(url);
+        const mapOfRounds = response.data;
         const data = this.state.data;
         const firstHalfGoals = [];
         const secondHalfGoals = [];
-        const mapGoalOfRound = new Map;
-        data.map((game) => {
-            game.round.map((round) => {
-                const numRound = game.round;
-                if (mapGoalOfRound.has(numRound)){
-                    mapGoalOfRound.set(numRound,mapGoalOfRound.get(numRound)+1);
-                } else {
-                    mapGoalOfRound.set(numRound,1);
-                }
-                game.goals.map((goal) => {
-                    if (goal.minute <=minuteOfHalf) {
-                        firstHalfGoals.push(goal);
-                    } else {
-                        secondHalfGoals.push(goal);
-                    }
-                })
+        const mapGoalsPerRound = new Map;
+
+        mapOfRounds.map((game) =>{
+            let counter = 0;
+            let round = 1;
+            game.goals.map((goal) => {
+                            if (goal.minute <= minuteOfHalf) {
+                                firstHalfGoals.push(goal);
+                                counter = counter+1;
+                            } else {
+                                secondHalfGoals.push(goal);
+                                counter = counter+1;
+                            }
             })
+            mapGoalsPerRound.set(round, counter )
+            round = round +1;
         })
+        // data.map((game) => {
+        //     game.round.map((round) => {
+        //         const numRound = game.round;
+        //         if (mapGoalOfRound.has(numRound)) {
+        //             mapGoalOfRound.set(numRound, mapGoalOfRound.get(numRound) + 1);
+        //         } else {
+        //             mapGoalOfRound.set(numRound, 1);
+        //         }
+        //         game.goals.map((goal) => {
+        //             if (goal.minute <= minuteOfHalf) {
+        //                 firstHalfGoals.push(goal);
+        //             } else {
+        //                 secondHalfGoals.push(goal);
+        //             }
+        //         })
+        //     })
+        // })
         this.setState({
             ...this.state,
             firstHalfGoals: firstHalfGoals,
             secondHalfGoals: secondHalfGoals,
-            mapGoalOfRound : mapGoalOfRound,
+            mapGoalsPerRound: mapGoalsPerRound,
         })
 
     }
